eaca03231e9235ec3c34220a5614539c1a75f8b1
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-09-11 19:39:12 +0300

    feat: Добавлен интерфейс админ сервиса и сервиса аутентификации

---
+++ src/main/java/com/example/bankcards/service/auth/AuthService.java
@@ -0,0 +1,16 @@
+package com.example.bankcards.service.auth;
+
+import com.example.bankcards.dto.user.UserAuthResponse;
+import com.example.bankcards.dto.user.UserLoginRequest;
+import com.example.bankcards.dto.user.UserRegistrationRequest;
+
+public interface AuthService {
+
+    UserAuthResponse registerUser(UserRegistrationRequest userRegistrationRequest);
+
+    UserAuthResponse loginUser(UserLoginRequest userLoginRequest);
+
+    UserAuthResponse updateRefreshToken(String refreshToken);
+
+    void logout(String refreshToken);
+}
