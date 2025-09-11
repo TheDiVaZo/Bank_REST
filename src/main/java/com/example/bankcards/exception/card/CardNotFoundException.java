8a94b415d1f2e252cc2a33890b5e0457454dffda
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-09-11 23:59:29 +0300

    refactor: Упрощены констркторы классов ошибок

--- src/main/java/com/example/bankcards/exception/card/CardNotFoundException.java
+++ src/main/java/com/example/bankcards/exception/card/CardNotFoundException.java
@@ -7,12 +7,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 @Getter
 public class CardNotFoundException extends RuntimeException {
-    private final String cardNumber;
-    private final String phoneNumber;
 
-    public CardNotFoundException(String cardNumber, String phoneNumber) {
-        super(String.format("Card '%s' not found for user '%s'", cardNumber, phoneNumber));
-        this.cardNumber = cardNumber;
-        this.phoneNumber = phoneNumber;
+    public CardNotFoundException(String message) {
+        super(message);
     }
 }
--- src/main/java/com/example/bankcards/exception/user/UserNotFoundException.java
+++ src/main/java/com/example/bankcards/exception/user/UserNotFoundException.java
@@ -8,4 +8,9 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
     }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
