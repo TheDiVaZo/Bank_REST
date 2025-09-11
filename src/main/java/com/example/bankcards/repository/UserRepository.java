d5ab3d000838161cfed4d1b2bc8f9f775b7af8de
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-09-11 22:38:04 +0300

    feat: В UserRepository добавлен метод на проверку существования refreshToken'а у какого-либо юзера.

--- src/main/java/com/example/bankcards/repository/UserRepository.java
+++ src/main/java/com/example/bankcards/repository/UserRepository.java
@@ -4,17 +4,19 @@ import com.example.bankcards.entity.Card;
 import com.example.bankcards.entity.User;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<Card> {
     Optional<User> findByPhoneNumber(String phoneNumber);
 
     @NotNull Optional<User> findById(@NotNull UUID id);
 
     Optional<User> findByRefreshToken(String refreshToken);
+
+    boolean existsByRefreshToken(String refreshToken);
 
     boolean existsByPhoneNumber(String phoneNumber);
 }
