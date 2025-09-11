64e5a860d38725a5103fca7e42151dad6ddfe44d
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-09-11 19:38:28 +0300

    refactor: Убрана зависимость получения кард от юзера

--- src/main/java/com/example/bankcards/repository/CardRepository.java
+++ src/main/java/com/example/bankcards/repository/CardRepository.java
@@ -2,6 +2,7 @@ package com.example.bankcards.repository;
 
 import com.example.bankcards.entity.Card;
 import com.example.bankcards.entity.User;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,11 +12,9 @@ import java.util.UUID;
 
 @Repository
 public interface CardRepository extends JpaRepository<Card, UUID> {
+    @NotNull Optional<Card> findById(@NotNull UUID id);
     Optional<Card> findByCardNumber(String cardNumber);
 
     List<Card> findByUser_Id(UUID userId);
     List<Card> findByUser_PhoneNumber(String userPhoneNumber);
-
-    Optional<Card> findByCardNumberAndUser_Id(String cardNumber, UUID userId);
-    Optional<Card> findByCardNumberAndUser_PhoneNumber(String cardNumber, String userPhoneNumber);
 }
