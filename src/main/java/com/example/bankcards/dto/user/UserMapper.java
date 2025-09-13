4a5b957625ba5ae1135a4d26a92f03ebff1cb7a4
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-09-13 20:31:39 +0300

    fix: Исправил проблему с UserMapper

--- src/main/java/com/example/bankcards/dto/user/UserMapper.java
+++ src/main/java/com/example/bankcards/dto/user/UserMapper.java
@@ -1,6 +1,9 @@
 package com.example.bankcards.dto.user;
 
 import com.example.bankcards.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
@@ -13,15 +16,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
         },
         uses = {
                 PasswordEncoder.class,
-        }
+        },
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR
 )
 public abstract class UserMapper {
 
-    private final PasswordEncoder passwordEncoder;
-
-    protected UserMapper(PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
+    private PasswordEncoder passwordEncoder;
 
     public abstract UserDto toDto(User user);
 
@@ -29,7 +29,7 @@ public abstract class UserMapper {
     @Mapping(target = "refreshTokenExpiry", ignore = true)
     @Mapping(target = "refreshToken", ignore = true)
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "createdAt", expression = "java(java.time.LocalDate.now())")
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     @Mapping(target = "cards", ignore = true)
     @Mapping(target = "password", expression = "java(passwordEncoder.encode(userRegistrationRequest.getPassword()))")
     public abstract User fromUserRegistrationRequest(UserRegistrationRequest userRegistrationRequest);
